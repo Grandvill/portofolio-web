560def8e08475ff8a9d702ce929cb0fce484e3ce
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-08-20 12:59:41 +0700

    Add : wild oasis project detail

--- src/components/projectDetail/ProjectDetail.tsx
+++ src/components/projectDetail/ProjectDetail.tsx
@@ -1,12 +1,14 @@
 import { useParams } from 'react-router-dom';
 import WildOasis from './WildOasis';
 import ITFreelanceHub from './ITFreelanceHub';
+import WildOasisNext from './WildOasisNext';
 
 export default function ProjectDetail() {
   const { id } = useParams();
 
-  if (id === 'wild-oasis') return <WildOasis />;
   if (id === 'itfreelancehub') return <ITFreelanceHub />;
+  if (id === 'wild-oasis') return <WildOasis />;
+  if (id === 'wild-oasis-next') return <WildOasisNext />;
 
   return <h2 className="text-center mt-10 text-red-500">Project not found</h2>;
 }
