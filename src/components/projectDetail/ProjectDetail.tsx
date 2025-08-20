03bf55f6520ee65a84210b38d99832703b012578
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-08-20 19:07:10 +0700

    Add : project detail sikembar

--- src/components/projectDetail/ProjectDetail.tsx
+++ src/components/projectDetail/ProjectDetail.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import WildOasis from './WildOasis';
 import ITFreelanceHub from './ITFreelanceHub';
 import WildOasisNext from './WildOasisNext';
+import Sikembar from './Sikembar';
 
 export default function ProjectDetail() {
   const { id } = useParams();
@@ -9,6 +10,7 @@ export default function ProjectDetail() {
   if (id === 'itfreelancehub') return <ITFreelanceHub />;
   if (id === 'wild-oasis') return <WildOasis />;
   if (id === 'wild-oasis-next') return <WildOasisNext />;
+  if (id === 'sikembar') return <Sikembar />;
 
   return <h2 className="text-center mt-10 text-red-500">Project not found</h2>;
 }
